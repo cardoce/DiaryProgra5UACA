fb92e8e3a50cb63e5433739fd31052d777d9661e
Fernando Cardoce <fcardoce@pernix-solutions.com>
2019-04-09 13:19:33 -0600

    Add association of events and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190409031450) do
+ActiveRecord::Schema.define(version: 20190409042913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20190409031450) do
     t.string "descritption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe User do
+    it { should have_many :events}
+  end
 end
